98e807bb8fcba56616ec64e8f0e2c3afe20220b6
nhatvu148 <nhatvu148@gmail.com>
2021-06-26 14:06:29 +0900

    MapHealthChecks

--- Startup.cs
+++ Startup.cs
@@ -52,6 +52,7 @@ namespace Catalog
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +74,7 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
