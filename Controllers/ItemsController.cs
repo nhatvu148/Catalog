02d7a60e514a78aea08f7344e4acc077ee626887
nhatvu148 <nhatvu148@gmail.com>
2021-06-26 12:23:56 +0900

    update

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -58,5 +58,28 @@ namespace Catalog.Controllers
 
             return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
         }
+
+        // PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            // Copy record
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
     }
 }
